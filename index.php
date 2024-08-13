2ed2fd41f69cafd8a3066d7454400ace4043bd1e
TolMen <cotton.frachisse@gmail.com>
2024-08-13 16:04:03 +0200

    Access to the BDD

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'src/BDDControl/connectBDD.php';
+
 if(empty($_SERVER['QUERY_STRING'])) {
     header("Location: src/views/homePage/home.php");
     exit;
---
+++ src/BDDControl/connectBDD.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+
+$host = 'localhost';
+$dbname = 'ocprojet5';
+$admin = 'root';
+$pass = '';
+
+include 'src/model/log.php';
+
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8;", $admin, $pass);
+} catch (PDOException $e) {
+    $message = "ERREUR Base de donnée indisponible : " . $e->getMessage() . PHP_EOL . PHP_EOL;
+    writeLog($message, "LogFiles/error.log");
+    die;
+}
