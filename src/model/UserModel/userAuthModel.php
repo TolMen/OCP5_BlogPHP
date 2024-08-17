f6ef7ee6c82806eb5fb1eba8edbd7b0057b01b52
TolMen <cotton.frachisse@gmail.com>
2024-08-17 20:03:29 +0200

    Establishment of MVC architecture

---
+++ src/model/UserModel/userAuthModel.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+- Inclusion de fichier nécessaire :
+Connexion à la BDD
+- Necessary file inclusion :
+Connection to the database
+*/
+require_once '../BDDControl/connectBDD.php'; 
+
+/*
+- Classe pour gérer les opérations de connexion
+- Class to manage connection operations
+*/
+class UserAuthModel {
+
+    /*
+    - Cette fonction récupère les informations utilisateurs en effectuant une liaison à la BDD, puis prépare une requête SQL de sélection, l'exécute afin de retourner les données sous forme de tableau associatif
+    - This function retrieves user information by connecting to the database, then prepares an SQL selection query, executes it to return the data in the form of an associative table
+    */
+    public function getAuthUser($pseudo) {
+        global $bdd; 
+        
+        $recupUser = $bdd->prepare('SELECT * FROM users WHERE pseudo = ?');
+
+        $recupUser->execute([$pseudo]);
+
+        return $recupUser->fetch();
+    }
+}
