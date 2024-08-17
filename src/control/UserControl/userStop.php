da8b4d36a4c5bd3031280ace6492c84b2949cdc8
TolMen <cotton.frachisse@gmail.com>
2024-08-17 20:04:34 +0200

    Editing self-documentation

--- src/control/UserControl/userStop.php
+++ src/control/UserControl/userStop.php
@@ -1,15 +1,37 @@
 <?php
 
+/* 
+- Reprend une session existante
+- Resumes an existing session
+*/
 session_start();
 
+/* 
+- Inclusion des fichiers nécessaire :
+Modèle gestion utilisateur + logs 
+- Inclusion of necessary files:
+User management + logs model
+*/
 require_once '../BDDControl/connectBDD.php';
 require_once '../../model/log.php';
 
-// User logout
+/*
+- Réinitialise les variables, puis détruit la session
+- Resets variables, then destroys the session
+*/
 $_SESSION = array();
 session_destroy();
 
+/*
+- Gestion des logs par un message et un appel de fonction
+- Logs management by a message and a function call
+*/
 $message = "Info : Déconnexion réussie pour un utilisateur - " . date("d-m-Y H:i:s") . PHP_EOL . PHP_EOL;
 writeLog($message, "../../../LogFiles/logout.log");
 
+/*
+- Redirection vers la page d'accueil des visiteurs
+- Redirect to the visitors' home page
+*/
 header('Location: ../../views/Page/home.php');
+exit();
