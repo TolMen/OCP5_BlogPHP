2712c5bfe6dcea7c81831572b391b8eef86c8142
TolMen <cotton.frachisse@gmail.com>
2024-08-18 17:27:39 +0200

    Adding a favicon to respond to SymfonyInsight

--- src/views/Page/home.php
+++ src/views/Page/home.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="mobile-web-app-capable" content="yes" />
         <!-- Useful tag -->
-        <link rel="icon" type="image/png" sizes="16x16" href="../../../assets/img/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../../../public/favicon-16x16.png" />
         <title>HomePage</title>
         <meta
             name="description"
