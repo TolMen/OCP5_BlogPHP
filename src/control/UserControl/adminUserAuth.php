80a37a32109cdd93ee37b5dd96b25f064daf8e90
TolMen <cotton.frachisse@gmail.com>
2024-08-26 15:14:19 +0200

    Controller part for login for administrator

---
+++ src/control/UserControl/adminUserAuth.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+- Inclusion des fichiers nécessaire
+- Inclusion of necessary files
+*/
+require_once '../../model/LogModel/logWriteModel.php';
+$config = require dirname(__DIR__, 3) . '/config/configAdmin.php';
+
+/*
+- Extraction des informations administrateur
+- Extraction of administrator informations
+*/
+$adminPseudo = $config['adminPseudo'];
+$adminPassword = $config['adminPassword'];
+
+/*
+- Vérification des identifiants administrateur
+- Admin credentials verification
+*/
+if ($pseudo === $adminPseudo && $mdp === $adminPassword) {
+    /*
+    - Stock les informations dans des variables de session
+    - Store the information in session variables
+    */
+    $_SESSION['pseudo'] = $pseudo;
+    $_SESSION['role'] = 'admin';
+
+    /*
+    - Gestion des logs par un message et un appel de fonction
+    - Logs management by a message and a function call
+    */
+    $message = "Connexion réussie pour l'administrateur '{$pseudo}' - " . date("d-m-Y H:i:s") . PHP_EOL . PHP_EOL;
+    writeLog($message, "../../../LogFiles/login.log");
+
+    /*
+    - Redirection vers la page d'administration
+    - Redirect to the admin page
+    */
+    header('Location: ../../../README.md');
+    exit();
+}
