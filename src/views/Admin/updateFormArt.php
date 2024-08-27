d4251a06f8ec69485dba90e4b1e9a6b8cbe6cd55
TolMen <cotton.frachisse@gmail.com>
2024-08-27 10:57:40 +0200

    Added links to use CSS (Please note this is based on the controller and not html)

--- src/views/Admin/updateFormArt.php
+++ src/views/Admin/updateFormArt.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier l'Article</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <!-- Useful tag -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../assets/img/logo.png" />
+    <title>Edit Article</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../views/CSS/styleAdmin/updateFormArt.css">
 </head>
 <body>
     <!-- Section to edit an article -->
     <section class="editArt">
         <h2>Modifier l'article</h2>
-        <div class="formeditArt">
+        <div class="formEditArt">
             <form method="POST" action="../../control/ArtControl/updateArt.php?id=<?php echo $article['id']; ?>">
                 <div class="inputBox">
                     <span>Titre</span>
